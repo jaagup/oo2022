128b7227d263b4129af0d50414f65b00e134fc2b
andressikka <50819370+andressikka@users.noreply.github.com>
2022-05-27 02:25:45 +0300

    Upload

---
+++ 11_proovikontrolltoo2/src/Jook.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Constructor;
+
+public class Jook {
+    String nimetus;
+    Double liitriHind;
+    Double tihedus;
+
+    public Jook(String nimetus, Double liitriHind, Double tihedus){
+        this.nimetus = nimetus;
+        this.liitriHind = liitriHind;
+        this.tihedus = tihedus;
+    }
+
+
+
+    // 997 кг/м³ - вода
+    // 1030 кг/м3. - кока кола
+    // 949 кг/м3 - водка
+}
---
+++ 11_proovikontrolltoo2/src/Kast.java
@@ -0,0 +1,11 @@
+public class Kast {
+    String kastityyp;
+    Double kastihind;
+    Double kastimass;
+    Integer pesadeArv;
+
+    public Kast(String kastityyp, Integer pesadeArv){
+        this.kastityyp = kastityyp;
+        this.pesadeArv = pesadeArv;
+    }
+}
